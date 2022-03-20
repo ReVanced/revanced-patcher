5ecbbfc9541f356e9030a1f8d9650cf211d4ca16
Lucaskyy <contact@sculas.xyz>
2022-03-20 20:32:17 +0100

    chore: fix credentials for publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,8 +33,8 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/ReVancedTeam/revanced-patcher")
             credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
             }
         }
     }
