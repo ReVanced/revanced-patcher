d5a3c76389ba902c22ddc8b7ba1a110b7ff852df
Lucaskyy <contact@sculas.xyz>
2022-03-20 15:54:41 +0100

    fix: avoid ignoring test resources (fixes #1)

---
+++ src/test/kotlin/net/revanced/patcher/util/TestUtil.kt
@@ -0,0 +1,14 @@
+package net.revanced.patcher.util
+
+import org.objectweb.asm.tree.AbstractInsnNode
+import kotlin.test.assertTrue
+
+object TestUtil {
+    fun <T: AbstractInsnNode> assertNodeEqual(a: T, b: T) {
+        assertTrue()
+    }
+
+    private fun AbstractInsnNode.nodeString(): String {
+
+    }
+}
