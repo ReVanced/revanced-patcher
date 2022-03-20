6c0f0823c91dc643dd80205b1e840e59827bee06
Lucaskyy <contact@sculas.xyz>
2022-03-20 22:04:53 +0100

    fix: Patch should have access to the Cache
    BREAKING CHANGE: Method signature of execute() was changed to include the cache, this will break existing implementations of the Patch class.

--- src/main/kotlin/net/revanced/patcher/patch/Patch.kt
+++ src/main/kotlin/net/revanced/patcher/patch/Patch.kt
@@ -1,5 +1,7 @@
 package net.revanced.patcher.patch
 
+import net.revanced.patcher.cache.Cache
+
 abstract class Patch(val patchName: String) {
-    abstract fun execute(): PatchResult
+    abstract fun execute(cache: Cache): PatchResult
 }
