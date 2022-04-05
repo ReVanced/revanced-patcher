66a9b768457e98fdde0b61f9a8d6aed4c1872027
oSumAtrIX <johan.melkonyan1@web.de>
2022-04-06 01:23:53 +0200

    fix: return mutable set of classes

--- src/main/kotlin/app/revanced/patcher/Patcher.kt
+++ src/main/kotlin/app/revanced/patcher/Patcher.kt
@@ -45,7 +45,7 @@ class Patcher(
                         cache.classes.add(proxy.mutatedClass)
                     }
 
-                return setOf(cache.classes)
+                return cache.classes
             }
 
             override fun getOpcodes(): Opcodes {
