e4157332d3bf0aacd36ceaf614c0840492a81049
oSumAtrIX <johan.melkonyan1@web.de>
2022-04-06 23:09:16 +0200

    add: `MutableMethodImplementation.addInstructions` extension

--- src/main/kotlin/app/revanced/patcher/extensions/Extensions.kt
+++ src/main/kotlin/app/revanced/patcher/extensions/Extensions.kt
@@ -1,5 +1,13 @@
 package app.revanced.patcher.extensions
 
 import org.jf.dexlib2.AccessFlags
+import org.jf.dexlib2.builder.BuilderInstruction
+import org.jf.dexlib2.builder.MutableMethodImplementation
 
 infix fun AccessFlags.or(other: AccessFlags) = this.value or other.value
+
+fun MutableMethodImplementation.addInstructions(index: Int, instructions: List<BuilderInstruction>) {
+    for (i in instructions.lastIndex downTo 0) {
+        this.addInstruction(index, instructions[i])
+    }
+}
