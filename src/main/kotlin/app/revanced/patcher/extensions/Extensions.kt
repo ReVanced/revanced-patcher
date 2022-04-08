b44bf4c2670fad6174d3692a83c7f6351f1c45e3
Lucaskyy <contact@sculas.xyz>
2022-04-08 22:55:12 +0200

    chore: move replace extension method to Extensions.kt

--- src/main/kotlin/app/revanced/patcher/extensions/Extensions.kt
+++ src/main/kotlin/app/revanced/patcher/extensions/Extensions.kt
@@ -3,6 +3,7 @@ package app.revanced.patcher.extensions
 import org.jf.dexlib2.AccessFlags
 import org.jf.dexlib2.builder.BuilderInstruction
 import org.jf.dexlib2.builder.MutableMethodImplementation
+import org.jf.dexlib2.iface.ClassDef
 
 infix fun AccessFlags.or(other: AccessFlags) = this.value or other.value
 
@@ -10,4 +11,9 @@ fun MutableMethodImplementation.addInstructions(index: Int, instructions: List<B
     for (i in instructions.lastIndex downTo 0) {
         this.addInstruction(index, instructions[i])
     }
+}
+
+internal fun MutableSet<ClassDef>.replace(originalIndex: Int, mutatedClass: ClassDef) {
+    this.remove(this.elementAt(originalIndex))
+    this.add(mutatedClass)
 }
