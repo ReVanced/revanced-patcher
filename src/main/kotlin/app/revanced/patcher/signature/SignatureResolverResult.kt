c63b20fa65aba8bb060a4a7a652747cba7198c2b
Lucaskyy <contact@sculas.xyz>
2022-04-08 23:51:31 +0200

    feat: add immutableMethod
    added docs

--- src/main/kotlin/app/revanced/patcher/signature/SignatureResolverResult.kt
+++ src/main/kotlin/app/revanced/patcher/signature/SignatureResolverResult.kt
@@ -2,19 +2,36 @@ package app.revanced.patcher.signature
 
 import app.revanced.patcher.proxy.ClassProxy
 import app.revanced.patcher.signature.resolver.SignatureResolver
+import org.jf.dexlib2.iface.Method
 
 /**
  * Represents the result of a [SignatureResolver].
  * @param definingClassProxy The [ClassProxy] that the matching method was found in.
  * @param resolvedMethodName The name of the actual matching method.
- * @param scanData OpCodes pattern scan result.
+ * @param scanData Opcodes pattern scan result.
  */
 data class SignatureResolverResult(
     val definingClassProxy: ClassProxy,
     val scanData: PatternScanResult,
     private val resolvedMethodName: String,
 ) {
+    /**
+     * Returns the **mutable** method by the [resolvedMethodName] from the [definingClassProxy].
+     *
+     * Please note, this method creates a [ClassProxy].
+     * Use [immutableMethod] where possible.
+     */
     val method get() = definingClassProxy.resolve().methods.first {
+        it.name == resolvedMethodName
+    }
+
+    /**
+     * Returns the **immutable** method by the [resolvedMethodName] from the [definingClassProxy].
+     *
+     * If you need to modify the method, use [method] instead.
+     */
+    val immutableMethod: Method
+        get() = definingClassProxy.immutableClass.methods.first {
         it.name == resolvedMethodName
     }
 
