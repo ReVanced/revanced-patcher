e161f7fea449883b7ac0fb436ed4f7f2ff78af62
Lucaskyy <contact@sculas.xyz>
2022-04-14 11:59:23 +0200

    revert: AccessFlag extensions not working with IDE

--- src/main/kotlin/app/revanced/patcher/extensions/Extensions.kt
+++ src/main/kotlin/app/revanced/patcher/extensions/Extensions.kt
@@ -4,12 +4,8 @@ import org.jf.dexlib2.AccessFlags
 import org.jf.dexlib2.builder.BuilderInstruction
 import org.jf.dexlib2.builder.MutableMethodImplementation
 
-class AccessFlagExtensions {
-    companion object {
-        infix fun AccessFlags.or(other: AccessFlags) = this.value or other.value
-        infix fun Int.or(other: AccessFlags) = this or other.value
-    }
-}
+infix fun AccessFlags.or(other: AccessFlags) = this.value or other.value
+infix fun Int.or(other: AccessFlags) = this or other.value
 
 fun MutableMethodImplementation.addInstructions(index: Int, instructions: List<BuilderInstruction>) {
     for (i in instructions.lastIndex downTo 0) {
