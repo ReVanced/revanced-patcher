a0d6d462170552929039d71eafa813fdfde215cb
Lucaskyy <contact@sculas.xyz>
2022-04-14 16:42:51 +0200

    fix: Suppress unused for addFiles

--- src/main/kotlin/app/revanced/patcher/Patcher.kt
+++ src/main/kotlin/app/revanced/patcher/Patcher.kt
@@ -41,6 +41,7 @@ class Patcher(
      * @param allowedOverwrites A list of class types that are allowed to be overwritten.
      * @param throwOnDuplicates If this is set to true, the patcher will throw an exception if a duplicate class has been found.
      */
+    @Suppress("unused")
     fun addFiles(
         files: Iterable<File>,
         allowedOverwrites: Iterable<String> = emptyList(),
