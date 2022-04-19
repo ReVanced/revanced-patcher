c245edb0c5317c1bb884ea315a1a04b720f20dd5
oSumAtrIX <johan.melkonyan1@web.de>
2022-04-19 21:51:50 +0200

    feat: string signature (#22)
    * feat: string signature
    
    Signed-off-by: oSumAtrIX <johan.melkonyan1@web.de>
    
    * fix: signature in test
    
    Signed-off-by: oSumAtrIX <johan.melkonyan1@web.de>
    
    * fix: make string signature optional
    
    Signed-off-by: oSumAtrIX <johan.melkonyan1@web.de>
    
    * fix: use of `compareOpcodes` when comparing string signatures
    
    Signed-off-by: oSumAtrIX <johan.melkonyan1@web.de>
    
    * add: `PackageMetadata` for signatures
    
    Signed-off-by: oSumAtrIX <johan.melkonyan1@web.de>

--- src/main/kotlin/app/revanced/patcher/signature/MethodSignature.kt
+++ src/main/kotlin/app/revanced/patcher/signature/MethodSignature.kt
@@ -11,6 +11,7 @@ import org.jf.dexlib2.Opcode
  * @param accessFlags The access flags of the method.
  * @param methodParameters The parameters of the method.
  * @param opcodes The list of opcodes of the method.
+ * @param strings A list of strings which a method contains.
  * A `null` opcode is equals to an unknown opcode.
  */
 class MethodSignature(
@@ -18,7 +19,8 @@ class MethodSignature(
     internal val returnType: String?,
     internal val accessFlags: Int?,
     internal val methodParameters: Iterable<String>?,
-    internal val opcodes: Iterable<Opcode?>?
+    internal val opcodes: Iterable<Opcode?>?,
+    internal val strings: Iterable<String>? = null
 ) {
     /**
      * The result of the signature
