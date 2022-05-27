3f0c740200dd91a060426638c2f8f516938b4c53
oSumAtrIX <johan.melkonyan1@web.de>
2022-05-27 14:26:06 +0200

    fix: `JarPatchBundle` loading non-class files to class loader

--- src/main/kotlin/app/revanced/patcher/util/patch/implementation/JarPatchBundle.kt
+++ src/main/kotlin/app/revanced/patcher/util/patch/implementation/JarPatchBundle.kt
@@ -10,9 +10,21 @@ import java.util.jar.JarFile
  * @param patchBundlePath The path to the patch bundle.
  */
 class JarPatchBundle(patchBundlePath: String) : PatchBundle(patchBundlePath) {
-    fun loadPatches() = loadPatches(URLClassLoader(arrayOf(this.toURI().toURL()), null), StringIterator(
-        JarFile(this).entries().iterator()
-    ) {
-        it.realName.replace('/', '.').replace(".class", "")
-    })
+    fun loadPatches() = loadPatches(
+        URLClassLoader(
+            arrayOf(this.toURI().toURL()),
+            Thread.currentThread().contextClassLoader // TODO: find out why this is required
+        ),
+        StringIterator(
+            JarFile(this)
+                .entries()
+                .toList() // TODO: find a cleaner solution than that to filter non class files
+                .filter {
+                    it.name.endsWith(".class") && !it.name.contains("$")
+                }
+                .iterator()
+        ) {
+            it.realName.replace('/', '.').replace(".class", "")
+        }
+    )
 }
