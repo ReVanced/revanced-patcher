6628b7870fc052da40be0d50a7e2b0b6c57743cc
Lucaskyy <contact@sculas.xyz>
2022-06-21 21:02:50 +0200

    fix: add docs (trigger release)

--- src/main/kotlin/app/revanced/patcher/PatcherResult.kt
+++ src/main/kotlin/app/revanced/patcher/PatcherResult.kt
@@ -7,6 +7,7 @@ import brut.directory.ExtFile
  * The result of a patcher.
  * @param dexFiles The patched dex files.
  * @param doNotCompress List of relative paths to files to exclude from compressing.
+ * @param resourceFile ExtFile containing resources that need to be extracted into the APK.
  */
 data class PatcherResult(
     val dexFiles: List<DexFile>,
