6f72c4c4c051e48c8d03d2a7b2cfc1c53028ed86
oSumAtrIX <johan.melkonyan1@web.de>
2022-07-02 17:44:08 +0200

    feat: streams overload for `XmlFileHolder`

--- src/main/kotlin/app/revanced/patcher/data/impl/ResourceData.kt
+++ src/main/kotlin/app/revanced/patcher/data/impl/ResourceData.kt
@@ -4,6 +4,8 @@ import app.revanced.patcher.data.Data
 import org.w3c.dom.Document
 import java.io.Closeable
 import java.io.File
+import java.io.InputStream
+import java.io.OutputStream
 import javax.xml.parsers.DocumentBuilderFactory
 import javax.xml.transform.TransformerFactory
 import javax.xml.transform.dom.DOMSource
@@ -17,14 +19,20 @@ class ResourceData(private val resourceCacheDirectory: File) : Data, Iterable<Fi
     override fun iterator() = resourceCacheDirectory.walkTopDown().iterator()
 
     inner class XmlFileHolder {
+        operator fun get(inputStream: InputStream, outputStream: OutputStream) =
+            DomFileEditor(inputStream, outputStream)
+
         operator fun get(path: String) = DomFileEditor(this@ResourceData[path])
     }
 }
 
-class DomFileEditor internal constructor(private val domFile: File) : Closeable {
-    val file: Document = DocumentBuilderFactory.newInstance().newDocumentBuilder()
-        .parse(domFile).also(Document::normalize)
+class DomFileEditor internal constructor(inputStream: InputStream, private val outputStream: OutputStream) : Closeable {
+    constructor(file: File) : this(file.inputStream(), file.outputStream())
 
-    override fun close() = TransformerFactory.newInstance().newTransformer()
-        .transform(DOMSource(file), StreamResult(domFile.outputStream()))
+    val file: Document =
+        DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(inputStream).also(Document::normalize)
+
+    override fun close() =
+        TransformerFactory.newInstance().newTransformer().transform(DOMSource(file), StreamResult(outputStream))
+
 }
