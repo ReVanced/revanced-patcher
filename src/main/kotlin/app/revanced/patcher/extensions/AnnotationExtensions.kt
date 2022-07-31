523f67b238646caaa9b7676a0e238ce82adbdda4
Sculas <contact@sculas.xyz>
2022-07-31 15:07:23 +0200

    feat: add Patch#dependsOn extension

--- src/main/kotlin/app/revanced/patcher/extensions/AnnotationExtensions.kt
+++ src/main/kotlin/app/revanced/patcher/extensions/AnnotationExtensions.kt
@@ -36,13 +36,18 @@ private fun <T : Annotation> Class<*>.findAnnotationRecursively(
 }
 
 object PatchExtensions {
-    val Class<out Patch<Data>>.patchName: String
-        get() = recursiveAnnotation(Name::class)?.name ?: this.javaClass.simpleName
+    val Class<out Patch<Data>>.patchName: String get() = recursiveAnnotation(Name::class)?.name ?: this.javaClass.simpleName
     val Class<out Patch<Data>>.version get() = recursiveAnnotation(Version::class)?.version
     val Class<out Patch<Data>>.include get() = recursiveAnnotation(app.revanced.patcher.patch.annotations.Patch::class)!!.include
     val Class<out Patch<Data>>.description get() = recursiveAnnotation(Description::class)?.description
     val Class<out Patch<Data>>.dependencies get() = recursiveAnnotation(app.revanced.patcher.patch.annotations.Dependencies::class)?.dependencies
     val Class<out Patch<Data>>.compatiblePackages get() = recursiveAnnotation(Compatibility::class)?.compatiblePackages
+
+    @JvmStatic
+    fun Class<out Patch<Data>>.dependsOn(patch: Class<out Patch<Data>>): Boolean {
+        if (this.patchName == patch.patchName) throw IllegalArgumentException("thisval and patch may not be the same")
+        return this.dependencies?.any { it.java.patchName == this@dependsOn.patchName } == true
+    }
 }
 
 object MethodFingerprintExtensions {
