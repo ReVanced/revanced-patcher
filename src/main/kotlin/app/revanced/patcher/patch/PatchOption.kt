e7abd25d6b714ffbda122fcdfc18c52bdb4267e4
Sculas <contact@sculas.xyz>
2022-08-02 00:21:05 +0200

    docs: add docs for PatchOptions

--- src/main/kotlin/app/revanced/patcher/patch/PatchOption.kt
+++ src/main/kotlin/app/revanced/patcher/patch/PatchOption.kt
@@ -1,17 +1,30 @@
 package app.revanced.patcher.patch
 
+/**
+ * A [Patch] option.
+ * @param key Unique identifier of the option. Example: _`settings.microg.enabled`_
+ * @param default The default value of the option.
+ * @param title A human-readable title of the option. Example: _MicroG Settings_
+ * @param description A human-readable description of the option. Example: _Settings integration for MicroG._
+ * @param required Whether the option is required.
+ */
+@Suppress("MemberVisibilityCanBePrivate")
 sealed class PatchOption<T>(
     val key: String,
-    default: T,
+    default: T?,
     val title: String,
     val description: String,
-    val required: Boolean = false
+    val required: Boolean
 ) {
-    var value: T = default
+    var value: T? = default
 
+    /**
+     * A [PatchOption] of type [String].
+     * @see PatchOption
+     */
     class StringOption(
         key: String,
-        default: String,
+        default: String?,
         title: String,
         description: String,
         required: Boolean = false
@@ -19,9 +32,13 @@ sealed class PatchOption<T>(
         key, default, title, description, required
     )
 
+    /**
+     * A [PatchOption] of type [Boolean].
+     * @see PatchOption
+     */
     class BooleanOption(
         key: String,
-        default: Boolean,
+        default: Boolean?,
         title: String,
         description: String,
         required: Boolean = false
@@ -29,9 +46,14 @@ sealed class PatchOption<T>(
         key, default, title, description, required
     )
 
+    /**
+     * A [PatchOption] with a list of allowed options.
+     * @param options A list of allowed options for the option.
+     * @see PatchOption
+     */
     sealed class ListOption<E>(
         key: String,
-        default: E,
+        default: E?,
         val options: Iterable<E>,
         title: String,
         description: String,
@@ -41,14 +63,18 @@ sealed class PatchOption<T>(
     ) {
         init {
             if (default !in options) {
-                throw IllegalStateException("Default option must be in options list")
+                throw IllegalStateException("Default option must be an allowed options")
             }
         }
     }
 
+    /**
+     * A [ListOption] of type [String].
+     * @see ListOption
+     */
     class StringListOption(
         key: String,
-        default: String,
+        default: String?,
         options: Iterable<String>,
         title: String,
         description: String,
@@ -57,9 +83,13 @@ sealed class PatchOption<T>(
         key, default, options, title, description, required
     )
 
+    /**
+     * A [ListOption] of type [Int].
+     * @see ListOption
+     */
     class IntListOption(
         key: String,
-        default: Int,
+        default: Int?,
         options: Iterable<Int>,
         title: String,
         description: String,
