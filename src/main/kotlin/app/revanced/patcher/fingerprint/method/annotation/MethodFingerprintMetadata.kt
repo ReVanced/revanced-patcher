8c502448be7f419fe5362696a317f6f364f6599d
oSumAtrIX <johan.melkonyan1@web.de>
2022-10-04 08:19:50 +0200

    feat!: remove unused annotation `MatchingMethod`

--- src/main/kotlin/app/revanced/patcher/fingerprint/method/annotation/MethodFingerprintMetadata.kt
+++ src/main/kotlin/app/revanced/patcher/fingerprint/method/annotation/MethodFingerprintMetadata.kt
@@ -3,18 +3,6 @@ package app.revanced.patcher.fingerprint.method.annotation
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 
 /**
- * Annotations for a method which matches to a [MethodFingerprint].
- * @param definingClass The defining class name of the method.
- * @param name A suggestive name for the method which the [MethodFingerprint] was created for.
- */
-@Target(AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class MatchingMethod(
-    val definingClass: String = "L<unspecified-class>;",
-    val name: String = "<unspecified-method>"
-)
-
-/**
  * Annotations to scan a pattern [MethodFingerprint] with fuzzy algorithm.
  * @param threshold if [threshold] or more of the opcodes do not match, skip.
  */
