0e72bfb96f266c3e6fe5d2e7e437a1e63dfce3e2
oSumAtrIX <johan.melkonyan1@web.de>
2022-10-11 16:29:11 +0200

    fix: visibility of constructor properties

--- src/main/kotlin/app/revanced/patcher/PatcherContext.kt
+++ src/main/kotlin/app/revanced/patcher/PatcherContext.kt
@@ -5,8 +5,8 @@ import org.jf.dexlib2.iface.ClassDef
 import java.io.File
 
 data class PatcherContext(
-    val classes: MutableList<ClassDef>,
-    val resourceCacheDirectory: File,
+    private val classes: MutableList<ClassDef>,
+    private val resourceCacheDirectory: File,
 ) {
     internal val patches = mutableListOf<Class<out Patch<Context>>>()
     internal val bytecodeContext = BytecodeContext(classes)
