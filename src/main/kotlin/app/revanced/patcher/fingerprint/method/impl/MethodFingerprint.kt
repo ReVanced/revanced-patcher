93ccd6caf7331c8872d438a784f73c914b627f84
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-06-11 22:46:44 +0400

    comments

--- src/main/kotlin/app/revanced/patcher/fingerprint/method/impl/MethodFingerprint.kt
+++ src/main/kotlin/app/revanced/patcher/fingerprint/method/impl/MethodFingerprint.kt
@@ -82,7 +82,7 @@ abstract class MethodFingerprint(
             val maxSignatureParameters = 5
             // Must append a unique value before the parameters to distinguish this key includes the parameters.
             // If this is not appended, then methods with no parameters
-            // will collide with the keys that specify only the access and return type.
+            // will collide with different keys that specify access/return and no signature.
             append("p:")
             parameters.forEachIndexed { index, parameter ->
                 if (index >= maxSignatureParameters) return
