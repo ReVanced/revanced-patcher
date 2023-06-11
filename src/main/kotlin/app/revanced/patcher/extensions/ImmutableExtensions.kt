689186edc0b5db5910808bc0abd002c8b032352a
1fexd <PGFvy85nC@protonmail.com>
2023-06-11 17:02:43 +0200

    feat!: Move matches extension method on ImmutableMethodReference to referenceEquals on ReferenceInstruction

--- src/main/kotlin/app/revanced/patcher/extensions/ImmutableExtensions.kt
+++ src/main/kotlin/app/revanced/patcher/extensions/ImmutableExtensions.kt
@@ -2,7 +2,6 @@ package app.revanced.patcher.extensions
 
 import org.jf.dexlib2.iface.instruction.ReferenceInstruction
 import org.jf.dexlib2.iface.reference.MethodReference
-import org.jf.dexlib2.immutable.reference.ImmutableMethodReference
 import java.util.*
 
 object ImmutableExtensions {
@@ -14,16 +13,16 @@ object ImmutableExtensions {
         }
     }
 
-    fun ImmutableMethodReference.matches(
-        instruction: ReferenceInstruction,
+    fun ReferenceInstruction.referenceEquals(
+        methodReference: MethodReference,
         match: EnumSet<MethodReferenceMatch> = MethodReferenceMatch.all
     ): Boolean {
-        val ref = (instruction.reference as? MethodReference) ?: return false
+        val ref = (reference as? MethodReference) ?: return false
 
-        if (match.contains(MethodReferenceMatch.DEFINING_CLASS) && ref.definingClass != definingClass) return false
-        if (match.contains(MethodReferenceMatch.NAME) && ref.name != name) return false
-        if (match.contains(MethodReferenceMatch.PARAMTER_TYPES) && ref.parameterTypes != parameterTypes) return false
-        if (match.contains(MethodReferenceMatch.RETURN_TYPE) && ref.returnType != returnType) return false
+        if (match.contains(MethodReferenceMatch.DEFINING_CLASS) && ref.definingClass != methodReference.definingClass) return false
+        if (match.contains(MethodReferenceMatch.NAME) && ref.name != methodReference.name) return false
+        if (match.contains(MethodReferenceMatch.PARAMTER_TYPES) && ref.parameterTypes != methodReference.parameterTypes) return false
+        if (match.contains(MethodReferenceMatch.RETURN_TYPE) && ref.returnType != methodReference.returnType) return false
 
         return true
     }
