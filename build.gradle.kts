433fe3af9f1b1996f159b3e344ba0d05c64d6ec4
oSumAtrIX <johan.melkonyan1@web.de>
2023-08-12 02:23:03 +0200

    build(Needs bump): Bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
     implementation("xpp3:xpp3:1.1.4c")
     implementation("com.android.tools.smali:smali:3.0.3")
     implementation("app.revanced:multidexlib2:3.0.3.r2")
-    implementation("app.revanced:apktool-lib:2.8.2-2")
+    implementation("app.revanced:apktool-lib:2.8.2-3")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.22")
     testImplementation("org.jetbrains.kotlin:kotlin-test:1.8.20-RC")
