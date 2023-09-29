4ad595c4662843af5e7329636dfb9427b362179a
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-09-29 16:45:55 +0400

    chore: remove unused suppress annotations

--- revanced-patcher/src/main/kotlin/app/revanced/patcher/patch/PatchResult.kt
+++ revanced-patcher/src/main/kotlin/app/revanced/patcher/patch/PatchResult.kt
@@ -6,5 +6,4 @@ package app.revanced.patcher.patch
  * @param patch The [Patch] that was executed.
  * @param exception The [PatchException] thrown, if any.
  */
-@Suppress("MemberVisibilityCanBePrivate")
 class PatchResult internal constructor(val patch: Patch<*>, val exception: PatchException? = null)
