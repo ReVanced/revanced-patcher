3e95e4937337a2a53896d7e2225527df2a8d7bdc
Pg <pg.developper.fr@gmail.com>
2025-04-23 01:15:33 +0200

    fix: Surrogate pairs by explicitly encoding utf-16 java string
    Unicode may has high value that overflows the 16bits range, java
    encodes char using 16bits, thus those high Unicode are spitted in two
    16bits chars called surrogate form. Unfortunately, aapt2 do not handle
    Unicode in surrogate form and crash when apktool try to repack the res.
    Explicitly telling the transformer to handle 16bits solve the issue.
    
    Co-authored-by: kitadai31 <90122968+kitadai31@users.noreply.github.com>
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

--- src/main/kotlin/app/revanced/patcher/util/Document.kt
+++ src/main/kotlin/app/revanced/patcher/util/Document.kt
@@ -4,7 +4,9 @@ import org.w3c.dom.Document
 import java.io.Closeable
 import java.io.File
 import java.io.InputStream
+import java.io.StringWriter
 import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.transform.OutputKeys
 import javax.xml.transform.TransformerFactory
 import javax.xml.transform.dom.DOMSource
 import javax.xml.transform.stream.StreamResult
@@ -35,14 +37,19 @@ class Document internal constructor(
             }
 
             it.outputStream().buffered().use { stream ->
-                TransformerFactory.newInstance()
-                    .newTransformer()
-                    .transform(DOMSource(this), StreamResult(stream))
+                val transformer = TransformerFactory.newInstance().newTransformer()
+                // Set to UTF-16 but encode as UTF-8 to prevent surrogate pairs from being escaped to broken numeric character references.
+                if (isAndroid) {
+                    transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-16")
+                    transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes")
+                }
+                transformer.transform(DOMSource(this), StreamResult(stream))
             }
         }
     }
 
     private companion object {
         private val readerCount = mutableMapOf<File, Int>()
+        private val isAndroid = System.getProperty("java.runtime.name").equals("Android Runtime")
     }
 }
